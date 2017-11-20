3ec303c9a962578500e560e35d629ab0c27af00e
Gilbert <gilbert.torchon@esih.edu>
2017-11-20 08:51:22 -0500

    2 player version almost done
    Now when the game is draw or when one of the player win the game shows it.
    Game also restarts automatically but this functionnality has bugs.

--- js/index.js
+++ js/index.js
@@ -8,6 +8,17 @@ $(document).ready(function(){
   var player1LetterChoice="X";
   var player2LetterChoice="O";
   var playerLetterChoice=player1LetterChoice;
+  var caseNonVide=0;
+  var leftDiagonal="";
+  var rightDiagonal="";
+  var leftVertical="";
+  var centerVertical="";
+  var rightVertical="";
+  var topHorizontal="";
+  var bottomHorizontal="";
+  var middleHorizontal="";
+  var player1WinningCoefficientTracker="1";
+  var player2WinningCoefficientTracker="2";
   var ticTacToeTable=[
     [0,0,0],
     [0,0,0],
@@ -112,8 +123,6 @@ $(document).ready(function(){
 
 //Creation du Tic-tac-toe en se servant des parametres de l'utilisatuer
 function createGame(){
-
-
   function updateTicTacToeGame(row,col){
     // alert("Player1 Letter Choice:"+player1LetterChoice);
     // alert("ligne:"+row+"Colomne"+col);
@@ -125,12 +134,9 @@ function createGame(){
       ticTacToeTable[row][col]=2;
     }
   }
-
-
-
   var clickCoordinates="";
   var row="";
-  var col=""
+  var col="";
   $("#tic-tac-toe-ui td").click(function(){
     if($(this).html()==="")
     {
@@ -138,6 +144,7 @@ function createGame(){
       clickCoordinates=$(this).attr('id').replace(/row-(\d)-col-(\d)/,"$1$2");// tranform id "row-1-col-2" to "12" for example
       row=clickCoordinates[0];
       col=clickCoordinates[1];
+      caseNonVide++;
       checkGameState(row,col);
       updateTicTacToeGame(row,col);
       playerLetterChoice=(playerLetterChoice===player1LetterChoice)?player2LetterChoice:player1LetterChoice;
@@ -146,30 +153,19 @@ function createGame(){
 
 }
 
-var leftDiagonal="";
-var rightDiagonal="";
-var leftVertical="";
-var centerVertical="";
-var rightVertical="";
-var topHorizontal="";
-var bottomHorizontal="";
-var middleHorizontal="";
-
-var player1WinningCoefficientTracker="1";
-var player2WinningCoefficientTracker="2";
-
 function checkGameState(row,col){
   //Check Diagonals
+
   if(row===col)
   {
     if(playerLetterChoice===player1LetterChoice)
     {
-      leftDiagonal+="1";
+      leftDiagonal+=player1WinningCoefficientTracker;
       if(row==1 && col===1)// row 1 column 1 is common to both diagonals
         rightDiagonal+=player1WinningCoefficientTracker;
     }
     else{
-      leftDiagonal+="2";
+      leftDiagonal+=player2WinningCoefficientTracker;
       if(row==1 && col===1)
         rightDiagonal+=player2WinningCoefficientTracker;
     }
@@ -189,110 +185,122 @@ function checkGameState(row,col){
    switch(col)
    {
      case "0":
-     switch(row){
-       case "0":
-       case "1":
-       case "2":
          leftVertical+=(playerLetterChoice===player1LetterChoice)?player2WinningCoefficientTracker:player1WinningCoefficientTracker;
        break;
-     }
-     break;
      case "1":
-       switch(row){
-         case "0":
-         case "1":
-         case "2":
            centerVertical+=(playerLetterChoice===player1LetterChoice)?player1WinningCoefficientTracker:player2WinningCoefficientTracker;
-         break;
-       }
        break;
      case "2":
-       switch(row){
-         case "0":
-         case "1":
-         case "2":
            rightVertical+=(playerLetterChoice===player1LetterChoice)?player1WinningCoefficientTracker:player2WinningCoefficientTracker;
-         break;
-       }
        break;
-     break;
    }
   // console.log("Center Vertical:"+centerVertical);
   //check horizontals
   switch(row)
   {
     case "0":
-       switch(col)
-       {
-         case "0":
-         case "1":
-         case "2":
            topHorizontal+=(playerLetterChoice===player1LetterChoice)?player1WinningCoefficientTracker:player2WinningCoefficientTracker;
          break;
-       }
-      break;
     case "1":
-      switch(col)
-      {
-        case "0":
-        case "1":
-        case "2":
          middleHorizontal+=(playerLetterChoice===player1LetterChoice)?player1WinningCoefficientTracker:player2WinningCoefficientTracker;
         break;
-      }
-      break;
     case "2":
-    switch(col)
-      {
-        case "0":
-        case "1":
-        case "2":
           bottomHorizontal+=(playerLetterChoice===player1LetterChoice)?1:2;
         break;
-      }
-      break;
   }
   console.log("Top Horizontal:"+topHorizontal);
   console.log("Center Horizontal:"+middleHorizontal);
   console.log("Bottom Horizontal:"+bottomHorizontal);
-  win("111");
-  win("222");
-}
 
-function win(playerWinningCoefficient)
+ // $(".game-result-box td").html(gameResult());
+var gameResults=gameResult();
+if(gameResults!==undefined)
 {
-  if(topHorizontal===playerWinningCoefficient || middleHorizontal===playerWinningCoefficient || bottomHorizontal===playerWinningCoefficient){
-    if(playerWinningCoefficient==="111")
-    {
-      alert("Player One Won!");
-    }
-    else if(playerWinningCoefficient==="222"){
-      alert("Player Two Won!");
-    }
+  $(".game-results-box p").html(gameResults);
+  gameRestart();
+}
+
+}
+
+
+function gameResult()
+{
+  if(caseNonVide===9)
+  {
+    return "DRAW!!";
   }
 
-  if(leftVertical===playerWinningCoefficient || rightVertical===playerWinningCoefficient || centerVertical===playerWinningCoefficient)
+  var player1WinningCoefficient="111";
+  var player2WinningCoefficient="222";
+  //**Horizontal****
+  if(topHorizontal===player1WinningCoefficient || middleHorizontal===player1WinningCoefficient || bottomHorizontal===player1WinningCoefficient)
   {
-    if(playerWinningCoefficient==="111")
-    {
-      alert("Player One Won!");
-    }
-    else if(playerWinningCoefficient==="222"){
-      alert("Player Two Won!");
-    }
+    return player1Name+" Won!";
   }
 
-  if(rightDiagonal==playerWinningCoefficient || leftDiagonal===playerWinningCoefficient)
+  if(topHorizontal===player2WinningCoefficient || middleHorizontal===player2WinningCoefficient || bottomHorizontal===player2WinningCoefficient)
   {
-    if(playerWinningCoefficient==="111")
-    {
-      alert("Player One Won!");
-    }
-    else if(playerWinningCoefficient==="222"){
-      alert("Player Two Won!");
-    }
+    return player2Name+" Won!";
+  }
+  //**Vertical**
+  if(leftVertical===player1WinningCoefficient || centerVertical===player1WinningCoefficient || rightVertical===player1WinningCoefficient)
+  {
+    return player1Name+" Won!";
+  }
+
+  if(leftVertical===player2WinningCoefficient || centerVertical===player2WinningCoefficient || rightVertical===player2WinningCoefficient)
+  {
+    return player2Name+" Won!";
+  }
+
+  //**Diagonals**
+  if(leftDiagonal===player1WinningCoefficient || rightDiagonal===player1WinningCoefficient)
+  {
+    return player1Name+" Won!";
+  }
+
+  if(leftDiagonal===player2WinningCoefficient || rightDiagonal===player2WinningCoefficient)
+  {
+    return player2Name+" Won!";
   }
 }
+
+function gameRestart(){
+  currentPosition=0;
+  playWithAI=false;
+  player1Name="";
+  player2Name="AI";
+  player1LetterChoice="X";
+  player2LetterChoice="O";
+  playerLetterChoice=player1LetterChoice;
+  caseNonVide=0;
+  leftDiagonal="";
+  rightDiagonal="";
+  leftVertical="";
+  centerVertical="";
+  rightVertical="";
+  topHorizontal="";
+  bottomHorizontal="";
+  middleHorizontal="";
+  player1WinningCoefficientTracker="1";
+  player2WinningCoefficientTracker="2";
+  ticTacToeTable=[
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+  ];
+  $("#tic-tac-toe-ui td").html("");
+
+  // for(var i=0;i<navigation_map.length;i++)
+  // {
+  //   $("#"+navigation_map[i]).hide();
+  // }
+  //
+  // $("#"+navigation_map[0]).show();
+
+  $(".game-results-box p").html("");
+}
+
 
 createGame();
 })
